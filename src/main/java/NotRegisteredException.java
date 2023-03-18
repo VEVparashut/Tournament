e5e40ef12ed61302e00eddff7c45bb469346d998
Neto test <89373786148@yandex.ru>
2023-03-18 10:58:39 +0300

    initial commit

---
+++ src/main/java/NotRegisteredException.java
@@ -0,0 +1,4 @@
+public class NotRegisteredException extends RuntimeException {
+    public NotRegisteredException(String s) {
+    }
+}
---
+++ src/test/java/GameTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+    Game game = new Game();
+    Player player1 = new Player(1, "Катя", 36);
+    Player player2 = new Player(2, "Таня", 39);
+    Player player3 = new Player(3, "Лена", 53);
+    Player player4 = new Player(4, "Саша", 36);
+    Player player5 = new Player(5, "Андрей", 41);
+    Player player6 = new Player(6, "Леша", 47);
+
+    @Test
+    public void test1() {
+        game.register(player3);
+        game.register(player1);
+
+        int expected = game.round("Лена", "Катя");
+        int actual = 1;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test0() {
+        game.register(player1);
+        game.register(player4);
+
+        int expected = game.round("Катя", "Саша");
+        int actual = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2() {
+        game.register(player4);
+        game.register(player2);
+
+        int expected = game.round("Саша", "Таня");
+        int actual = 2;
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
